1087241355fc47dab68e1a8461fd659ff01d9591
ogternoski <ternoski.murilo@escola.pr.gov.br>
2026-07-02 12:33:24 +0000

    alteração

--- perguntas.js
+++ perguntas.js
@@ -2,19 +2,161 @@
 // Troque disciplina, pergunta e resposta pelos conteúdos que você criou.
 
 criaCartao(
-    'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+    'filosofia 1',
+    'oque é metodo pré socratico 1',
+    'é a primeira forma de pensamento racional ocidental 1'
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'filosofia 2',
+    'oque é cinismo 2',
+    'é o comportamento de quem desrespeita normas sociais e morais, agindo com descaramento, sarcasmo ou fingimento 2'
 )
 
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    'matematica 3',
+    'oque é matris 3',
+    ' um arranjo retangular de números (ou outros elementos) organizados em linhas e colunas. 3'
+)
+criaCartao(
+    'matematica 1',
+    'oque é regra de tres 1',
+    'é um método matemático simples usado para descobrir um valor desconhecido quando conhecemos outros três 1'
+)
+
+criaCartao(
+    'portugues 2',
+    'oque é um verbo 2',
+    'é a classe de palavras que indica ação, estado, mudança de estado ou fenômenos da natureza 2'
+)
+
+criaCartao(
+    'portugues 3',
+    'oque é predicado 3',
+    '  é tudo aquilo que se diz, declara ou informa sobre o sujeito. 3'
+)
+
+criaCartao(
+    'fisica 1',
+    'oque é força centripita1',
+    '"força fictícia" (ou inercial) que parece "puxar" um objeto para fora de uma trajetória curva 1'
+)
+
+criaCartao(
+    'filosofia 2',
+    'oque é cinismo 2',
+    'é o comportamento de quem desrespeita normas sociais e morais, agindo com descaramento, sarcasmo ou fingimento 2'
+)
+
+criaCartao(
+    'matematica 3',
+    'oque é matris 3',
+    ' um arranjo retangular de números (ou outros elementos) organizados em linhas e colunas. 3'
+)
+
+criaCartao(
+    'filosofia 1',
+    'oque é metodo pré socratico 1',
+    'é a primeira forma de pensamento racional ocidental 1'
+)
+
+criaCartao(
+    'filosofia 2',
+    'oque é cinismo 2',
+    'é o comportamento de quem desrespeita normas sociais e morais, agindo com descaramento, sarcasmo ou fingimento 2'
+)
+
+criaCartao(
+    'matematica 3',
+    'oque é matris 3',
+    ' um arranjo retangular de números (ou outros elementos) organizados em linhas e colunas. 3'
+)
+criaCartao(
+    'filosofia 1',
+    'oque é metodo pré socratico 1',
+    'é a primeira forma de pensamento racional ocidental 1'
+)
+
+criaCartao(
+    'filosofia 2',
+    'oque é cinismo 2',
+    'é o comportamento de quem desrespeita normas sociais e morais, agindo com descaramento, sarcasmo ou fingimento 2'
+)
+
+criaCartao(
+    'matematica 3',
+    'oque é matris 3',
+    ' um arranjo retangular de números (ou outros elementos) organizados em linhas e colunas. 3'
+)
+
+criaCartao(
+    'filosofia 1',
+    'oque é metodo pré socratico 1',
+    'é a primeira forma de pensamento racional ocidental 1'
+)
+
+criaCartao(
+    'filosofia 2',
+    'oque é cinismo 2',
+    'é o comportamento de quem desrespeita normas sociais e morais, agindo com descaramento, sarcasmo ou fingimento 2'
+)
+
+criaCartao(
+    'matematica 3',
+    'oque é matris 3',
+    ' um arranjo retangular de números (ou outros elementos) organizados em linhas e colunas. 3'
+)
+
+criaCartao(
+    'filosofia 1',
+    'oque é metodo pré socratico 1',
+    'é a primeira forma de pensamento racional ocidental 1'
+)
+
+criaCartao(
+    'filosofia 2',
+    'oque é cinismo 2',
+    'é o comportamento de quem desrespeita normas sociais e morais, agindo com descaramento, sarcasmo ou fingimento 2'
+)
+
+criaCartao(
+    'matematica 3',
+    'oque é matris 3',
+    ' um arranjo retangular de números (ou outros elementos) organizados em linhas e colunas. 3'
+)
+
+criaCartao(
+    'filosofia 1',
+    'oque é metodo pré socratico 1',
+    'é a primeira forma de pensamento racional ocidental 1'
+)
+
+criaCartao(
+    'filosofia 2',
+    'oque é cinismo 2',
+    'é o comportamento de quem desrespeita normas sociais e morais, agindo com descaramento, sarcasmo ou fingimento 2'
+)
+
+criaCartao(
+    'matematica 3',
+    'oque é matris 3',
+    ' um arranjo retangular de números (ou outros elementos) organizados em linhas e colunas. 3'
+)
+
+criaCartao(
+    'filosofia 1',
+    'oque é metodo pré socratico 1',
+    'é a primeira forma de pensamento racional ocidental 1'
+)
+
+criaCartao(
+    'filosofia 2',
+    'oque é cinismo 2',
+    'é o comportamento de quem desrespeita normas sociais e morais, agindo com descaramento, sarcasmo ou fingimento 2'
+)
+
+criaCartao(
+    'matematica 3',
+    'oque é matris 3',
+    ' um arranjo retangular de números (ou outros elementos) organizados em linhas e colunas. 3'
 )
